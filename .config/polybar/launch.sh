7067bbf40da692420c156f47761e3ae96d45d5ee
Michael Gerov <mhgerov@gmail.com>
2026-03-21 07:48:43 -0400

    add secondary monitor polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-# Kill existing bars
 killall -q polybar
 
-# Wait until processes are shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do
+  sleep 0.2
+done
 
-# Launch bar
-polybar main &
+PRIMARY="$(xrandr --query | awk '/ primary / {print $1; exit}')"
+
+if [ -z "$PRIMARY" ]; then
+  PRIMARY="$(polybar --list-monitors | awk -F: 'NR==1{print $1}')"
+fi
+
+polybar --list-monitors | awk -F: '{print $1}' | while read -r m; do
+  if [ "$m" = "$PRIMARY" ]; then
+    MONITOR="$m" polybar main -c "$HOME/.config/polybar/config.ini" &
+  else
+    MONITOR="$m" polybar secondary -c "$HOME/.config/polybar/config.ini" &
+  fi
+done
